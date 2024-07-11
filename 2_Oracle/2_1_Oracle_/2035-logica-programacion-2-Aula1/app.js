09c2fdeba18ee3ba93320818a8198d571f59daaa
Ancamilo404 <Ancamilo404@gmail.com>
2024-07-11 14:51:55 -0500

    Add files via upload

---
+++ 2_Oracle/2_1_Oracle_/2035-logica-programacion-2-Aula1/app.js
@@ -0,0 +1,8 @@
+    let titulo = document.querySelector('h1');
+    titulo.innerHTML = 'Juego nuemro secreto';
+
+    let parrafo = document.querySelector('p');
+    parrafo.innerHTML = 'Indique el nuemro del 1 al 10';
+
+    function intentoDeUsuario (){
+    }
